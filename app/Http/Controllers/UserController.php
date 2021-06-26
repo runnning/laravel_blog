7abf8a46fdbc8c8353165564084f527260defd4d
yanWN <2467634970@qq.com>
2021-06-26 19:28:24 +0800

    登入成功后导航逻辑

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -24,6 +25,7 @@ class UserController extends Controller
            'email'=>$request->email,
            'password'=>bcrypt($request->password),
        ]);
+       Auth::login($user);
        session()->flash('success','欢迎,你将在这里开启一段新的旅程~');
        return redirect()->route('users.show',[$user]);
    }
