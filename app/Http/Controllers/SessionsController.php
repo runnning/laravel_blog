908d3e315cbc0f5165257d34e5ee36698118093a
yanWN <2467634970@qq.com>
2021-06-26 23:26:59 +0800

    更改用户资料

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,7 +22,6 @@ class SessionsController extends Controller
             session()->flash('danger','很抱歉,你的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-        return;
     }
     public function destroy(){
         Auth::logout();
