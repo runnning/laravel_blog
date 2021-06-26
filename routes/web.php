d7e01f0f71da7b73e1ca7448c86f2e11043f45bb
yanWN <2467634970@qq.com>
2021-06-26 19:00:36 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SessionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +19,7 @@ Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 Route::get('signup',[UserController::class,'create'])->name('signup');
-
 Route::resource('users',UserController::class);
+Route::get('login',[SessionsController::class,'create'])->name('login');
+Route::post('login',[SessionsController::class,'store'])->name('login');
+Route::delete('logout',[SessionsController::class,'destroy'])->name('logout');
