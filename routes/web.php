6099982990d7c393fc010b8862a0631a6f0f6b15
yanWN <2467634970@qq.com>
2021-06-26 15:32:40 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use App\Http\Controllers\StaticPagesController;
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
+Route::get('signup',[UserController::class,'create'])->name('signup');
