258906640b740850a9555b078314f181000ba86c
yanWN <2467634970@qq.com>
2021-06-27 18:14:49 +0800

    管理员可以删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -8,8 +8,16 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class UserPolicy
 {
     use HandlesAuthorization;
+    //当前用户只能对自己进行更新
     public function update(User $currentUser,User $user): bool
     {
         return $currentUser->id===$user->id;
+    }
+    /*
+     * 判断是否是管理员
+     * 删除对象不是自己
+     * */
+    public function destroy(User $currentUser,User $user){
+        return $currentUser->is_admin&&$currentUser->id!=$user->id;
     }
 }
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -3,4 +3,12 @@
     <a href="{{route('users.show',$user)}}">
         {{$user->name}}
     </a>
+    {{-- 删除授权  --}}
+    @can('destroy',$user)
+        <form action="{{route('users.destroy',$user->id)}}" method="post" class="float-right">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+        </form>
+    @endcan
 </div>
