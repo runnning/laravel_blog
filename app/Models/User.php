4b339db55102c5d028a14056c1c64241180b0fe4
yanWN <2467634970@qq.com>
2021-07-03 17:33:12 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,9 +60,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
-    public function feed(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids=$this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at', 'desc');
     }
     //粉丝列表
     public function followers(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
