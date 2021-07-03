4972e91bcc2437501aa219d1a8bdcbca650ff492
yanWN <2467634970@qq.com>
2021-07-03 14:45:32 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,10 +56,40 @@ class User extends Authenticatable
             $user->activation_token = Str::random(10);
         });
     }
-    public function statuses(){
+    public function statuses(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
         return $this->hasMany(Status::class);
     }
-    public function feed(){
+    public function feed(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
         return $this->statuses()->orderBy('created_at','desc');
+    }
+    //粉丝列表
+    public function followers(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+    //关注人列表
+    public function followings(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+    //关注
+    public function follow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids=compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+    //取消关注
+    public function unfollow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids=compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    //判断用户是否关注
+    public function isFollowing($user_id){
+        return $this->followings->contains($user_id);
     }
 }
